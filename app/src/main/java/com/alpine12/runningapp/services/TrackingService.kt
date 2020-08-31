91964899c3c6ccd7a64732f01dfd0f6d5b7e3c05
alpine12 <iqballecip@gmail.com>
2020-08-31 21:39:22 +0700

    Foreground Service

--- app/src/main/java/com/alpine12/runningapp/services/TrackingService.kt
+++ app/src/main/java/com/alpine12/runningapp/services/TrackingService.kt
@@ -1,31 +1,94 @@
 package com.alpine12.runningapp.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleService
+import com.alpine12.runningapp.R
 import com.alpine12.runningapp.other.Constant.ACTION_PAUSE_SERVICE
+import com.alpine12.runningapp.other.Constant.ACTION_SHOW_TRACKING_FRAGMENT
 import com.alpine12.runningapp.other.Constant.ACTION_START_OR_RESUME_SERVICE
 import com.alpine12.runningapp.other.Constant.ACTION_STOP_SERVICE
+import com.alpine12.runningapp.other.Constant.NOTIFICATION_CHANEL_ID
+import com.alpine12.runningapp.other.Constant.NOTIFICATION_CHANEL_NAME
+import com.alpine12.runningapp.other.Constant.NOTIFICATION_CHANNEL_ID
+import com.alpine12.runningapp.ui.MainActivity
 import timber.log.Timber
 
-class TrackingService : LifecycleService(){
+class TrackingService : LifecycleService() {
 
+    var isFirstRun =  true
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
-            when(it.action){
-                ACTION_START_OR_RESUME_SERVICE->{
+            when (it.action) {
+                ACTION_START_OR_RESUME_SERVICE -> {
                     Timber.d("Started or resume service")
+                    if (isFirstRun){
+                        startForeGroundService()
+                        isFirstRun = false
+                    }else{
+                        Timber.d("Resuming Service . . . ")
+                    }
+
                 }
 
-                ACTION_PAUSE_SERVICE->{
+                ACTION_PAUSE_SERVICE -> {
                     Timber.d("Pause service")
                 }
 
-                ACTION_STOP_SERVICE->{
+                ACTION_STOP_SERVICE -> {
                     Timber.d("Stop service")
                 }
             }
         }
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun startForeGroundService() {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(
+            this, NOTIFICATION_CHANNEL_ID
+        )
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_baseline_directions_run_24)
+            .setContentTitle("Running App")
+            .setContentText("00:00:00")
+            .setContentIntent(getMainActivityPendingIntent())
+
+        startForeground(NOTIFICATION_CHANEL_ID, notificationBuilder.build())
+
+    }
+
+    private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
+        this,
+        0,
+        Intent(this, MainActivity::class.java).also {
+            it.action = ACTION_SHOW_TRACKING_FRAGMENT
+        },
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val chanel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            NOTIFICATION_CHANEL_NAME,
+            NotificationManager.IMPORTANCE_LOW
+        )
+        notificationManager.createNotificationChannel(chanel)
     }
 }
--- app/src/main/java/com/alpine12/runningapp/ui/MainActivity.kt
+++ app/src/main/java/com/alpine12/runningapp/ui/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.alpine12.runningapp.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.alpine12.runningapp.R
+import com.alpine12.runningapp.other.Constant.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigateToTrackingFragmentIfNeeded(intent)
 
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
 
@@ -26,6 +30,17 @@ class MainActivity : AppCompatActivity() {
                     }
                     else -> bottomNavigationView.visibility = View.GONE
                 }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?){
+        if (intent?.action == ACTION_SHOW_TRACKING_FRAGMENT){
+            navHostFragment.findNavController().navigate(R.id.action_global_tracking_fragment)
         }
     }
 }
