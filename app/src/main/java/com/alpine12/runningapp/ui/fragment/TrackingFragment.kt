0abefb4754cb861a3d5040a12d82ebd1d845012a
alpine12 <iqballecip@gmail.com>
2020-09-26 14:28:02 +0700

    Saving run

--- app/src/main/java/com/alpine12/runningapp/ui/fragment/TrackingFragment.kt
+++ app/src/main/java/com/alpine12/runningapp/ui/fragment/TrackingFragment.kt
@@ -2,13 +2,13 @@ package com.alpine12.runningapp.ui.fragment
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.alpine12.runningapp.R
+import com.alpine12.runningapp.db.Run
 import com.alpine12.runningapp.other.Constant.ACTION_PAUSE_SERVICE
 import com.alpine12.runningapp.other.Constant.ACTION_START_OR_RESUME_SERVICE
 import com.alpine12.runningapp.other.Constant.ACTION_STOP_SERVICE
@@ -17,16 +17,19 @@ import com.alpine12.runningapp.other.Constant.POLYLINE_COLOR
 import com.alpine12.runningapp.other.Constant.POLYLINE_WIDTH
 import com.alpine12.runningapp.other.TrackingUtility
 import com.alpine12.runningapp.services.Polyline
-
 import com.alpine12.runningapp.services.TrackingService
 import com.alpine12.runningapp.ui.viewmodel.MainViewModel
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.PolylineOptions
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
 import timber.log.Timber
+import java.util.*
+import kotlin.math.round
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
@@ -42,6 +45,9 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private var menu: Menu? = null
 
+    private var weight = 80f
+//    private var
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -56,9 +62,12 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         mapView.onCreate(
             savedInstanceState
         )
-
         btnToggleRun.setOnClickListener {
             toggleRun()
+        }
+        btnFinishRun.setOnClickListener {
+            zoomToSeeWholeTrack()
+            endRunAndSaeToDb()
         }
 
         mapView.getMapAsync {
@@ -90,7 +99,7 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private fun toggleRun() {
         if (isTracking) {
-            menu?.getItem(0)?.isVisible =true
+            menu?.getItem(0)?.isVisible = true
             sendCommandToService(ACTION_PAUSE_SERVICE)
         } else {
             sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
@@ -112,8 +121,8 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId){
-            R.id.miCancelTracking ->{
+        when (item.itemId) {
+            R.id.miCancelTracking -> {
                 showCancelTrackingDialog()
             }
         }
@@ -135,7 +144,7 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         dialog.show()
     }
 
-    private fun stopRun(){
+    private fun stopRun() {
         sendCommandToService(ACTION_STOP_SERVICE)
         findNavController().navigate(R.id.action_trackingFragment2_to_runFragment)
     }
@@ -147,7 +156,7 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
             btnFinishRun.visibility = View.VISIBLE
         } else {
             btnToggleRun.text = "Stop"
-            menu?.getItem(0)?.isVisible =true
+            menu?.getItem(0)?.isVisible = true
             btnFinishRun.visibility = View.GONE
         }
     }
@@ -160,6 +169,52 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
                     MAP_ZOOM
                 )
             )
+        }
+    }
+
+    private fun zoomToSeeWholeTrack() {
+        val bounds = LatLngBounds.Builder()
+        for (polyline in pathPoints) {
+            for (pos in polyline) {
+                bounds.include(pos)
+            }
+        }
+
+        map?.moveCamera(
+            CameraUpdateFactory.newLatLngBounds(
+                bounds.build(),
+                mapView.width,
+                mapView.height,
+                (mapView.height * 0.05f).toInt()
+            )
+        )
+    }
+
+    private fun endRunAndSaeToDb() {
+        map?.snapshot { bmp ->
+            var distanceInMeters = 0f
+            for (polyline in pathPoints) {
+                distanceInMeters += TrackingUtility.calculatePolylineLength(polyline).toInt()
+            }
+            val avgSpeed =
+                round((distanceInMeters / 1000f) / (currentTimeMillis / 1000f / 60 / 60) * 10) / 10f
+            val dateTImeStamp = Calendar.getInstance().timeInMillis
+            val caloriesBurned = ((distanceInMeters / 1000f) * weight).toInt()
+            val run = Run(
+                bmp,
+                dateTImeStamp,
+                avgSpeed,
+                distanceInMeters,
+                currentTimeMillis,
+                caloriesBurned
+            )
+            viewModel.insertRun(run)
+            Snackbar.make(
+                requireActivity().findViewById(R.id.rootView),
+                "Run saved successfully",
+                Snackbar.LENGTH_LONG
+            ).show()
+            stopRun()
         }
     }
 
--- app/src/main/java/com/alpine12/runningapp/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/alpine12/runningapp/ui/viewmodel/MainViewModel.kt
@@ -2,9 +2,16 @@ package com.alpine12.runningapp.ui.viewmodel
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.alpine12.runningapp.db.Run
 import com.alpine12.runningapp.repositories.MainRepository
+import kotlinx.coroutines.launch
 
 class MainViewModel @ViewModelInject constructor(
     val mainRepository: MainRepository
 ) : ViewModel() {
+
+    fun insertRun(run : Run) = viewModelScope.launch {
+        mainRepository.insertRun(run)
+    }
 }
