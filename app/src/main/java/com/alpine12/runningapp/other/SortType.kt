f37a6d103aba4938855e158a7b7313f43b2b95c1
alpine12 <iqballecip@gmail.com>
2020-09-28 22:10:02 +0700

    Shorting Runs

---
+++ app/src/main/java/com/alpine12/runningapp/other/SortType.kt
@@ -0,0 +1,7 @@
+package com.alpine12.runningapp.other
+
+enum class SortType {
+
+    DATE, RUNNING_TIME, AVG_SPEED, DISTANCE, CALORIES_BURN
+
+}
--- app/src/main/java/com/alpine12/runningapp/ui/fragment/RunFragment.kt
+++ app/src/main/java/com/alpine12/runningapp/ui/fragment/RunFragment.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.alpine12.runningapp.R
 import com.alpine12.runningapp.adapters.RunAdapter
 import com.alpine12.runningapp.other.Constant.REQUEST_CODE_LOCATION_PERMISSION
+import com.alpine12.runningapp.other.SortType
 import com.alpine12.runningapp.other.TrackingUtility
 import com.alpine12.runningapp.ui.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -31,7 +33,33 @@ class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionC
         super.onViewCreated(view, savedInstanceState)
         setupRecycleView()
 
-        viewModel.runsSortedByDate.observe(viewLifecycleOwner, Observer {
+        when (viewModel.sortType) {
+            SortType.DATE -> spFilter.setSelection(0)
+            SortType.RUNNING_TIME -> spFilter.setSelection(1)
+            SortType.DISTANCE -> spFilter.setSelection(2)
+            SortType.AVG_SPEED -> spFilter.setSelection(3)
+            SortType.CALORIES_BURN -> spFilter.setSelection(4)
+        }
+
+        spFilter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                pos: Int,
+                id: Long
+            ) {
+                when(pos){
+                    0 -> viewModel.sortRuns(SortType.DATE)
+                    1 -> viewModel.sortRuns(SortType.RUNNING_TIME)
+                    2 -> viewModel.sortRuns(SortType.DISTANCE)
+                    3 -> viewModel.sortRuns(SortType.AVG_SPEED)
+                    4 -> viewModel.sortRuns(SortType.CALORIES_BURN)
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+
+        viewModel.runs.observe(viewLifecycleOwner, Observer {
             runAdapter.submitList(it)
         })
 
